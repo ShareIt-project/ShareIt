94887695447e23d85443c4d456a2b0b556a75c9e
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2013-07-27 17:10:26 +0200

    Split html_basic in an own project

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,7 @@
 
 
 PRODUCTION_BRANCH="gh-pages"
-PRODUCTION_REMOVE="doc html_basic test_images COLLABORATE.md deploy.sh README.md"
-PRODUCTION_REMOVE=$(PRODUCTION_REMOVE)" lib/README.md js/shareit-core/doc"
+PRODUCTION_REMOVE="doc test_images COLLABORATE.md deploy.sh README.md lib/README.md"
 
 
 echo
